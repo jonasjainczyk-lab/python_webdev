f827448407f4fed8ae3b0b22450a0afc5353b38e
dusya001 <dusya001st@gmail.com>
2026-07-12 17:47:50 +0200

    removed debug help in details.html

--- mediahub/forms.py
+++ mediahub/forms.py
@@ -2,8 +2,6 @@ from django import forms
 from .models import UserRating
 
 
-RATING_CHOICES = [(i / 2, i / 2) for i in range(0, 21)]
-
 
 class UserRatingForm(forms.ModelForm):
     rating = forms.DecimalField(min_value=0,max_value=10,max_digits=3,decimal_places=1,)
